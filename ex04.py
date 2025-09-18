c82d2b9bdf525cc7e6888e1d843963f3aac2342a
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 17:34:31 +0300

    gitautosync update 101

---
+++ ex04.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# loading the module
+import argparse
+
+# creating the parser
+parser = argparse.ArgumentParser()
+
+# add arguments for name and company
+parser.add_argument('-1', '--first', type=int)
+parser.add_argument('-o', '--op')
+parser.add_argument('-2', '--second', type=int, default=10)
+
+# parse the arguments, putting them into a new namespace
+args = parser.parse_args()
+
+if args.op == '+':
+    result = args.first + args.second
+elif args.op == '-':
+    result = args.first - args.second
+else:
+    result = f'(Unknown operator {args.op})'
+
+print(f'{args.first} {args.op} {args.second} = {result}')
