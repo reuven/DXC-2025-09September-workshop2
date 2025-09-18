e9534cc1a6fd83fc1de2c9e2965b5785bb3bedf6
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 18:42:04 +0300

    gitautosync update 167

--- myargs08.py
+++ myargs08.py
@@ -14,6 +14,9 @@ args = parser.parse_args()
 
 for one_file in args.files:
     outfilename = one_file.name + '.output'  # Get output filename
+
+    print(f'Reversing {one_file.name} into {outfilename}...')
+
     with one_file as infile, open(outfilename, 'w') as outfile:
         for one_line in infile:
             outfile.write(one_line.rstrip()[::-1] + '\n')
