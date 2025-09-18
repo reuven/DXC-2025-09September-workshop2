f6a0f798d0402e6ac83d67186d87924ed7f66665
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 17:59:24 +0300

    gitautosync update 120

---
+++ ex05.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# loading the module
+import argparse
+
+# creating the parser
+parser = argparse.ArgumentParser(description='Simple calculator program')
+
+# add arguments for name and company
+parser.add_argument('-1', '--first', type=int, help='First number', required=True)
+parser.add_argument('-o', '--op', help='Operator', required=True)
+parser.add_argument('-2', '--second', type=int, default=10, help='Second number')
+
+# parse the arguments, putting them into a new namespace
+args = parser.parse_args()
+
+if args.op == '+':
+    result = args.first + args.second
+elif args.op == '-':
+    result = args.first - args.second
+else:
+    result = f'(Unknown operator {args.op})'
+
+print(f'{args.first} {args.op} {args.second} = {result}')
