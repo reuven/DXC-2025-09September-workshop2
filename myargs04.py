04311966521e13d634c24be59ecf751ca6ac9c4f
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 17:08:31 +0300

    gitautosync update 84

--- myargs04.py
+++ myargs04.py
@@ -11,10 +11,7 @@ parser.add_argument('first', type=int)
 parser.add_argument('op')
 parser.add_argument('second', type=int, default=10)
 
-parser.add_argument('age', type=float)
-
 # parse the arguments, putting them into a new namespace
 args = parser.parse_args()
 
 # grab the names via attributes on "args"
-print(f'Next year, {args.name} will be {args.age + 1} years old!')
