560f58474ba44563a141f8df46d5d6daa2ca77cd
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 18:30:10 +0300

    gitautosync update 139

--- ex06.py
+++ ex06.py
@@ -5,12 +5,10 @@
 import argparse
 
 # creating the parser
-parser = argparse.ArgumentParser(description='Simple calculator program')
+parser = argparse.ArgumentParser(description='Reverse lines of a file')
 
 # add arguments for name and company
 parser.add_argument('-1', '--first', type=int, help='First number', required=True)
-parser.add_argument('-o', '--op', help='Operator', required=True)
-parser.add_argument('-2', '--second', type=int, default=10, help='Second number')
 
 # parse the arguments, putting them into a new namespace
 args = parser.parse_args()
