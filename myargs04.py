e9a8649fd4ff07651c4e18705b47303398e6783a
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 17:16:24 +0300

    gitautosync update 95

--- myargs04.py
+++ myargs04.py
@@ -7,9 +7,9 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # add arguments for name and company
-parser.add_argument('-f', '--first', type=int)
+parser.add_argument('-1', '--first', type=int)
 parser.add_argument('-o', '--op')
-parser.add_argument('-s', '--second', type=int)
+parser.add_argument('-2', '--second', type=int)
 
 # parse the arguments, putting them into a new namespace
 args = parser.parse_args()
