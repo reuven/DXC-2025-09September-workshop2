79cac1b5d7e9c178405f9ea56ba67abbff3b08e7
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 16:31:03 +0300

    gitautosync update 59

---
+++ myargs03.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# loading the module
+import argparse
+
+# creating the parser
+parser = argparse.ArgumentParser()
+
+# add arguments for name and company
+parser.add_argument('name')
+parser.add_argument('age')
+
+# parse the arguments, putting them into a new namespace
+args = parser.parse_args()
+
+# grab the names via attributes on "args"
+print(f'Next year, {args.name} will be {args.age + 1} years old!')
