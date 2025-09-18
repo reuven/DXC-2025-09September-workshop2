60529f4c066ef4a14829d2b327ed4bba9931bdaa
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 17:41:25 +0300

    gitautosync update 113

--- myargs06.py
+++ myargs06.py
@@ -7,7 +7,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # add arguments
-parser.add_argument('filename')
+parser.add_argument('filename', type=file)
 
 # parse the arguments, putting them into a new namespace
 args = parser.parse_args()
