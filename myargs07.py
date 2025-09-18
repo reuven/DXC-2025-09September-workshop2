321e3c27b537240906e13333149d41bd22ab953d
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 18:38:45 +0300

    gitautosync update 157

---
+++ myargs07.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# loading the module
+import argparse
+
+# creating the parser
+parser = argparse.ArgumentParser()
+
+# add arguments
+parser.add_argument('file', type=argparse.FileType('r'))
+
+# parse the arguments, putting them into a new namespace
+args = parser.parse_args()
+
+for one_line in args.file:
+    print(len(one_line), end=' ')
