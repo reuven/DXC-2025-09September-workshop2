f69b7becfddb2d2ae5bbce6771e05a51f0ea94ee
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 16:26:32 +0300

    gitautosync update 48

---
+++ ex02.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# loading the module
+import argparse
+
+# creating the parser
+parser = argparse.ArgumentParser()
+
+# add arguments for name and company
+parser.add_argument('name')
+parser.add_argument('age')
+
+# parse the arguments, putting them into a new namespace
+args = parser.parse_args()
+
+# grab the names via attributes on "args"
+print(f'Hello, {args.name} from {args.company}!')
