24dedf2392dcac68b85dcdb995fe44e79a7476f3
Reuven Lerner <reuven@lerner.co.il>
2025-09-18 15:51:43 +0300

    gitautosync update 28

---
+++ ex01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# # Exercise: Simple greeting
+
+# 1. Write a command-line program that uses `sys.argv`.
+# 2. It expects to get two arguments, a person's name and their company name.
+# 3. It should print a greeting to the person using their company, too.
