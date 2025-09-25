0cec5a03b98446d19585a3c2cbd2c0c859dfc96e
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 15:24:10 +0300

    gitautosync update 22

--- mycmd.py
+++ mycmd.py
@@ -4,7 +4,7 @@ import cmd
 
 class MyCmd(cmd.Cmd):
     def do_say(self, line):
-        print(f'Hello, {line}')
+        print(f'I am saying: {line}')
 
 if __name__ == '__main__':
     MyCmd().cmdloop()
