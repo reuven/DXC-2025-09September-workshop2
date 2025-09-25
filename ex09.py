d224ebb640bb07e1aaabbfa935a91ed181919913
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 16:49:49 +0300

    gitautosync update 95

---
+++ ex09.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import cmd
+
+class Calculator(cmd.Cmd):
+    def line_to_numbers(self, line):
+        numbers = [int(one_number)
+                   for one_number in line.split()]
+        print(f'{numbers=}')
+        return numbers
+
+    def do_add(self, line):
+        """Add two numbers"""
+        numbers = self.line_to_numbers(line)
+        result = numbers[0] + numbers[1]
+        print(f'{numbers[0]} + {numbers[1]} = {result}')
+
+    def do_sub(self, line):
+        """Subtract two numbers"""
+        numbers = self.line_to_numbers(line)
+        result = numbers[0] - numbers[1]
+        print(f'{numbers[0]} - {numbers[1]} = {result}')
+
+    def do_mul(self, line):
+        """Multiply two numbers"""
+        numbers = self.line_to_numbers(line)
+        result = numbers[0] * numbers[1]
+        print(f'{numbers[0]} * {numbers[1]} = {result}')
+
+    def do_div(self, line):
+        """Divide two numbers"""
+        numbers = self.line_to_numbers(line)
+        result = numbers[0] / numbers[1]
+        print(f'{numbers[0]} / {numbers[1]} = {result}')
+
+    def do_EOF(self, line):
+        """Quit like a Unix geek"""
+        print('Goodbye!')
+        return True
+
+    def do_exit(self, line):
+        """Quit like a normal person"""
+        print('Goodbye, normal person!')
+        return True
+
+if __name__ == '__main__':
+    Calculator().cmdloop()
