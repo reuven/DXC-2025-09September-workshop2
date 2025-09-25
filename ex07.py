8956ab13e7c4918e295f4d7785118ff747da329e
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 15:51:08 +0300

    gitautosync update 39

--- ex07.py
+++ ex07.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
 import cmd
+
+class MyCmd(cmd.Cmd):
+    def do_say(self, line):
+        print(f'I am saying: {line}')
+
+    def do_EOF(self, line):
+        print('Goodbye!')
+        return True
+
+    def do_exit(self, line):
+        print('Goodbye, normal person!')
+        return True
+
+if __name__ == '__main__':
+    MyCmd().cmdloop()
