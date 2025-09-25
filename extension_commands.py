d151911ab9107f3381b904f558715ac339fee5d1
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 18:07:54 +0300

    gitautosync update 143

--- extension_commands.py
+++ extension_commands.py
@@ -1,3 +1,7 @@
 def say_with_stars(self, line):
     """Print to the user with *stars* before and after"""
     print(f'I am saying: **{line}**')
+
+def say_with_dollars(self, line):
+    """Print to the user with $$dollars$$ before and after"""
+    print(f'I am saying: **{line}**')
