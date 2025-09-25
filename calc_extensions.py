e8b5e7e66ac2c9bfe212899de4a9519fa0008879
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 18:37:00 +0300

    gitautosync update 169

--- calc_extensions.py
+++ calc_extensions.py
@@ -3,3 +3,9 @@ def cube(self, line):
     numbers = self.line_to_numbers(line)
     result = numbers[0] ** 3
     print(f'{numbers[0]} ** 3 = {result}')
+
+def negate(self, line):
+    """Get the negative of a number"""
+    numbers = self.line_to_numbers(line)
+    result = numbers[0] * -1
+    print(f'{numbers[0]} * -1 = {result}')
