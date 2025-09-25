1c7a76044a1b913d6b2af5fca4e628e25adb9a90
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 16:11:11 +0300

    gitautosync update 64

--- mycmd.py
+++ mycmd.py
@@ -4,13 +4,16 @@ import cmd
 
 class MyCmd(cmd.Cmd):
     def do_say(self, line):
+        """Say something to the user"""
         print(f'I am saying: {line}')
 
     def do_EOF(self, line):
+        """quit"""
         print('Goodbye!')
         return True
 
     def do_exit(self, line):
+        """quit like a non-Unix head"""
         print('Goodbye, normal person!')
         return True
 
