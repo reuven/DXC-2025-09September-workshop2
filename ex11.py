d7f86b5b5f59d5a9216aa0a8557c3577e50eb108
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 18:26:24 +0300

    gitautosync update 162

--- ex11.py
+++ ex11.py
@@ -67,7 +67,10 @@ if __name__ == '__main__':
     # setattr(Calculator, 'do_square', square)
     setattr(Calculator, 'do_square', lambda self, line: print(f'{self.line_to_numbers(line)[0] ** 2}'))
 
-    for one_function in dir(calc_extensions):
+    for function_name in dir(calc_extensions):
+        one_function = getattr(calc_extensions, function_name)
+
+        if callable(one_function):
         print(f'{one_function=}')
         setattr(Calculator, f'do_{one_function}', getattr(calc_extensions, one_function))
 
