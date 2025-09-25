15f89e4a429900810d94365a76e31fb34b0b8c7d
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 16:29:32 +0300

    gitautosync update 78

--- ex08.py
+++ ex08.py
@@ -10,25 +10,28 @@ class Calculator(cmd.Cmd):
         return numbers
 
     def do_add(self, line):
+        """Add two numbers"""
         numbers = self.line_to_numbers(line)
         result = numbers[0] + numbers[1]
         print(f'{numbers[0]} + {numbers[1]} = {result}')
 
     def do_sub(self, line):
+        """Subtract two numbers"""
         numbers = self.line_to_numbers(line)
         result = numbers[0] - numbers[1]
         print(f'{numbers[0]} - {numbers[1]} = {result}')
 
     def do_mul(self, line):
+        """Multiply two numbers"""
         numbers = self.line_to_numbers(line)
         result = numbers[0] * numbers[1]
         print(f'{numbers[0]} * {numbers[1]} = {result}')
 
     def do_div(self, line):
+        """Divide two numbers"""
         numbers = self.line_to_numbers(line)
         result = numbers[0] / numbers[1]
         print(f'{numbers[0]} / {numbers[1]} = {result}')
-
 
     def do_EOF(self, line):
         print('Goodbye!')
