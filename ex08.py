8c2664b23868cb219209ca428241882e9748681c
Reuven Lerner <reuven@lerner.co.il>
2025-09-25 16:26:57 +0300

    gitautosync update 70

---
+++ ex08.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import cmd
+
+class Calculator(cmd.Cmd):
+    def do_add(self, line):
+        numbers = [int(one_number)
+                   for one_number in line.split()]
+        print(f'add, {line=}, {numbers=}')
+        result = numbers[0] + numbers[1]
+        print(f'{numbers[0]} + {numbers[1]} = {result}')
+
+    def do_sub(self, line):
+        numbers = [int(one_number)
+                   for one_number in line.split()]
+        print(f'sub, {line=}, {numbers=}')
+        result = numbers[0] - numbers[1]
+        print(f'{numbers[0]} - {numbers[1]} = {result}')
+
+    def do_mul(self, line):
+        numbers = [int(one_number)
+                   for one_number in line.split()]
+        print(f'mul, {line=}, {numbers=}')
+        result = numbers[0] * numbers[1]
+        print(f'{numbers[0]} * {numbers[1]} = {result}')
+
+    def do_div(self, line):
+        numbers = [int(one_number)
+                   for one_number in line.split()]
+        print(f'div, {line=}, {numbers=}')
+        result = numbers[0] / numbers[1]
+        print(f'{numbers[0]} / {numbers[1]} = {result}')
+
+
+    def do_EOF(self, line):
+        print('Goodbye!')
+        return True
+
+    def do_exit(self, line):
+        print('Goodbye, normal person!')
+        return True
+
+if __name__ == '__main__':
+    Calculator().cmdloop()
